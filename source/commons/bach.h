f82198a68b45a5916c6481bc6b41413d7d62fa29
Andrea Agostini <and.agos@gmail.com>
2017-12-09 23:47:22 +0100

    windows build 0.8.0.1

--- source/commons/bach.h
+++ source/commons/bach.h
@@ -73,7 +73,7 @@
 			#else
 				#define CONFIGURATION_Deployment
 			#endif
-			#define BACH_LLLL_VERSION "0.8.0.1"
+			#define BACH_LLLL_VERSION "0.8.0.0"
 			#define BACH_VERSION "0.8.0.1"
 
 			#define MAXAPI_USE_MSCRT
