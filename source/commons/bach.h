f256c3e49825b253d07a5ef9f2f8296d9c4f2e77
Andrea Agostini <and.agos@gmail.com>
2019-03-20 16:36:49 +0100

    version number switched to 0.8.0.5

--- source/commons/bach.h
+++ source/commons/bach.h
@@ -74,7 +74,7 @@
 				#define CONFIGURATION_Deployment
 			#endif
 			#define BACH_LLLL_VERSION "0.8.0.0"
-			#define BACH_VERSION "0.8.1.0"
+			#define BACH_VERSION "0.8.0.5"
 
 			#define MAXAPI_USE_MSCRT
 		#endif
