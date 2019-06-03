997a3f7871647c735e026a528e2449db07fa2fc1
Daniele Ghisi <danieleghisi@gmail.com>
2019-06-03 23:06:59 +0200

    fixed bug causing frequent crashes with bach.minfo

--- source/mains/dg/minfo.c
+++ source/mains/dg/minfo.c
@@ -104,7 +104,7 @@ int T_EXPORT main()
 
 
     // no @out attribute, only versionnumber (hidden)
-    llllobj_class_add_versionnumber_attr(c, LLLL_OBJ_UI);
+    llllobj_class_add_versionnumber_attr(c, LLLL_OBJ_VANILLA);
 
 
 	CLASS_ATTR_CHAR(c, "matchtype", 0, t_minfo, match_type);
@@ -199,7 +199,7 @@ void minfo_inletinfo(t_minfo *x, void *b, long a, char *t)
 
 
 void minfo_free(t_minfo *x)
-{	
+{
 	llllobj_obj_free((t_llllobj_object *) x);
 }
 
