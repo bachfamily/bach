cc81601db59a3b60e9912f158c2f4ecb132821a1
Andrea Agostini <and.agos@gmail.com>
2019-09-27 13:14:28 +0200

    bach.prime: fixed memory corruption at instantiation

--- source/mains/math/prime.c
+++ source/mains/math/prime.c
@@ -64,7 +64,7 @@
 
 typedef struct _prime
 {
-	t_object	n_ob;
+	t_llllobj_object	n_ob;
 	void		*n_out;
 	long		n_prime;
 } t_prime;
@@ -91,7 +91,7 @@ void C74_EXPORT ext_main(void *moduleRef)
 	
 	if (llllobj_check_version(bach_get_current_llll_version()) || llllobj_test()) {
 		error("bach: bad installation");
-		return 1;
+		return;
 	}
 	
 	c = class_new("bach.prime", (method)prime_new, NULL, (short)sizeof(t_prime), 0L, A_GIMME, 0);
@@ -122,9 +122,7 @@ void C74_EXPORT ext_main(void *moduleRef)
 
 	dev_post("bach.prime compiled %s %s", __DATE__, __TIME__);
 
-    llllobj_class_add_versionnumber_attr(c, LLLL_OBJ_VANILLA);
-
-	return 0;
+	return;
 }
 
 void prime_bang(t_prime *x)
