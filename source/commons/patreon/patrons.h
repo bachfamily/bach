2c51bdc7fc8ca754185f26d77a8c899c00801b09
Daniele Ghisi <danieleghisi@gmail.com>
2020-07-12 15:54:51 +0200

    updated patrons, doctorMax run

--- source/commons/patreon/patrons.h
+++ source/commons/patreon/patrons.h
@@ -27,18 +27,24 @@
 
 void post_top_supporters()
 {
+	post("- Carmine Emanuele Cella");
 	post("- Francisco Colasanto");
+	post("- Isandro Ojeda");
 	post("- Jean-Baptiste Barrière");
 	post("- Julien Vincenot");
 	post("- Michele Zaccagnini");
 	post("- Seidenberg");
 	post("- Yan Maresz");
+	post("- Yoshiaki Onishi");
 }
 
 void post_all_patrons()
 {
-	post("Alessandro Ratoci, audiophil, Chris Chandler, Chris Poovey, Christopher Michael Trapani, Francisco Colasanto, Hans Leeuw, ");
-	post("Hans Tutschku, Jean-Baptiste Barrière, Jean-Julien Filatriau, Joost Van kerkhoven, Julien Vincenot, linazero, Louis Goldford, ");
-	post("Matthew Goodheart, Michele Zaccagnini, Nikola Kołodziejczyk, OH Jung-Woong, Pierre Alexandre Tremblay, Seidenberg, Tj Shredder, ");
-	post("Tomislav Oliver, Vens R., Viktor Velthuijs, Yan Maresz.");
+	post("Alessandro Ratoci, Alvaro, Andrew Milne, Billy Newman, Carmine Emanuele Cella, Chris Chandler, Chris Poovey, Christopher Lane, ");
+	post("Christopher Michael Trapani, Daniel, Douglas Geers, Francisco Colasanto, Gabriel José Bolaños, Gratkowski, Gustavo Chritaro, ");
+	post("Hans Leeuw, Hans Tutschku, Isandro Ojeda, Jean-Baptiste Barrière, Jean-Julien Filatriau, Jeremy Hunt, Johannes MacDonald, ");
+	post("Joost Van kerkhoven, Jose Miguel Fernandez, Joseph Norman, José Martínez, Julien Vincenot, Kenneth Stewart, Lotus Open Factory, ");
+	post("Louis Goldford, Mario Estrada Mari, Mark Gorrie, Mathilde, Matthew Goodheart, Michele Zaccagnini, Nikola Kołodziejczyk, ");
+	post("Pierre Alexandre Tremblay, Robert Appleton, Rune Glerup, Sean Ferguson, Sebastián García Surianu, Seidenberg, Seth Shafer, ");
+	post("Tj Shredder, Tomislav Oliver, Vens R., Viktor Velthuijs, Yan Maresz, Yoshiaki Onishi, audiophil, linazero, micron.");
 }
