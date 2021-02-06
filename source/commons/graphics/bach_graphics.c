05bf42cef7a451de6fcea5e9be7f466f11add59d
Daniele Ghisi <danieleghisi@gmail.com>
2021-02-06 17:33:14 +0100

    fixed bug potentially causing crashes with label families

--- source/commons/graphics/bach_graphics.c
+++ source/commons/graphics/bach_graphics.c
@@ -4397,7 +4397,6 @@ t_beziercs *get_venn_enclosure(long num_pts_in, t_pt *pts_in, long num_pts_out, 
     bach_freeptr(done);
     bach_freeptr(new_pts);
     bach_freeptr(pts);
-	bach_freeptr(done);
     polygon_free(p);
     polygon_free(q);
 
