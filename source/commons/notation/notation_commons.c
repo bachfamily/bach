0f00c4ad5575e6771ccfecf0336d779cbbeb71fb
Daniele Ghisi <danieleghisi@gmail.com>
2021-04-20 18:30:19 +0200

    fixed bug that broke the "sel chord"/"sel note" + path syntax, chore in help files

--- source/commons/notation/notation_commons.c
+++ source/commons/notation/notation_commons.c
@@ -21199,7 +21199,7 @@ t_chord *chord_get_from_path(t_notation_obj *r_ob, long voice_num, long meas_num
             chord_num = voice->num_chords + chord_num + 1;
 
         if (chord_num > 0 && chord_num <= voice->num_chords)
-            return nth_chord_of_rollvoice(voice, chord_num);
+            return nth_chord_of_rollvoice(voice, chord_num - 1);
         
     } else if (r_ob->obj_type == k_NOTATION_OBJECT_SCORE) {
         t_scorevoice *voice = (t_scorevoice *)nth_voice_safe(r_ob, voice_num - 1);
@@ -21235,7 +21235,7 @@ t_note *note_get_from_path(t_notation_obj *r_ob, long voice_num, long meas_num, 
             chord_num = voice->num_chords + chord_num + 1;
         
         if (chord_num > 0 && chord_num <= voice->num_chords) {
-            t_chord *temp = nth_chord_of_rollvoice(voice, chord_num);
+            t_chord *temp = nth_chord_of_rollvoice(voice, chord_num - 1);
             if (temp) {
                 if (note_num < 0)
                     note_num = temp->num_notes + note_num + 1;
