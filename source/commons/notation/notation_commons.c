eb0a6bb286249cc32f252229daa88a12dd5dbe5b
Daniele Ghisi <danieleghisi@gmail.com>
2024-11-15 17:26:29 +0100

    fixed issue with bigts

--- source/commons/notation/notation_commons.c
+++ source/commons/notation/notation_commons.c
@@ -303,7 +303,7 @@ char measure_has_visible_tempo(t_measure *meas)
 void paint_timesignature(t_notation_obj *r_ob, t_jgraphics* g, t_jrgba color, t_jfont *jf_ts, long clef, double staff_top, t_measure *curr_meas, char big)
 {
     long i;
-    int num_ts = ((clef == k_CLEF_FGG) || (clef == k_CLEF_FFG) || (clef == k_CLEF_FFGG) || (clef == k_CLEF_FG)) ? 2 : 1; // number of time signatures to paint
+    int num_ts = big ? 1 : (((clef == k_CLEF_FGG) || (clef == k_CLEF_FFG) || (clef == k_CLEF_FFGG) || (clef == k_CLEF_FG)) ? 2 : 1); // number of time signatures to paint
     double *tsbox_y1a = (double *) bach_newptr(num_ts * sizeof(double));
     double *tsbox_y1b = (double *) bach_newptr(num_ts * sizeof(double));
     double tsbox_x1 = unscaled_xposition_to_xposition(r_ob, (curr_meas->tuttipoint_reference ? curr_meas->tuttipoint_reference->offset_ux : 0) + curr_meas->start_barline_offset_ux) + (curr_meas->prev ? (measure_get_barline_uwidth(r_ob, curr_meas->prev) + r_ob->uwidth_after_barline_with_ts) * r_ob->zoom_y : 0);
