3816e4c15ec829ebd99638528e12674963d2f335
Andrea Agostini <andrea.agostini@conservatoriotorino.eu>
2026-07-10 11:09:26 +0200

    version number updated

--- source/commons/foundation/bach.h
+++ source/commons/foundation/bach.h
@@ -81,7 +81,7 @@
         #define CONFIGURATION_Deployment
     #endif
     #define BACH_LLLL_VERSION "0.8.0.0"
-    #define BACH_VERSION "0.8.9.0"
+    #define BACH_VERSION "0.8.9.1"
 
     #define MAXAPI_USE_MSCRT
 #endif
